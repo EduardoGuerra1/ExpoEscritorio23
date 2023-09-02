3d7c691e580e597c176c3d7e7feb50736c19aef8
Daniel <20190652@ricaldone.edu.sv>
2023-09-01 20:19:07 -0600

    Obervaciones Ya sirve todo god

--- src/View/Application/form/other/Observaciones.java
+++ src/View/Application/form/other/Observaciones.java
@@ -9,13 +9,17 @@ import View.samplemessage.Message;
 import com.formdev.flatlaf.FlatClientProperties;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLaf;
+import expoescritorio.Controller.ControllerFull;
+import static expoescritorio.Controller.Funciones.GetInasisitencias;
 import static expoescritorio.Controller.Funciones.GetObservaciones;
+import expoescritorio.Models.Inasisitenciastring;
 import expoescritorio.Models.ObservacionesString;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -47,39 +51,42 @@ public class Observaciones extends javax.swing.JPanel {
         
         lb.putClientProperty(FlatClientProperties.STYLE, ""
                 + "font:$h1.font");
-        
-        cargarDatos();
+        // Obtén el modelo de la tabla existente
+        DefaultTableModel tableModel = (DefaultTableModel) table1.getModel();
+        // Establece los "ColumnIdentifiers" en el modelo de la tabla
+        tableModel.setColumnIdentifiers(new Object[]{"ID", "Estudiante", "Docente", "Detalle","Fecha"});
+        cargarDatosAsync();
         
     }
-
-    public void cargarDatos(){
-        CompletableFuture<List<ObservacionesString>> futureObservaciones = GetObservaciones();
-        futureObservaciones.thenAccept(obsList -> {
+    public void deleteAllTableRows(JTable table) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        while (model.getRowCount() > 0) {
+            model.removeRow(0);
+        }
+    }
+   public void cargarDatosAsync() {
+    GetObservaciones()
+        .thenAccept(encargadosList -> {
             DefaultTableModel tableModel = (DefaultTableModel) table1.getModel();
-            // Agregar las columnas al modelo de tabla
-            tableModel.addColumn("ID Observacion");
-            tableModel.addColumn("Estudiante");
-            tableModel.addColumn("Docente");
-            tableModel.addColumn("Periodo");
-            tableModel.addColumn("Fecha");
-            tableModel.addColumn("Detalle");
-            tableModel.addColumn("Estudiante");
-
-            // Agregar los datos de Observaciones a la tabla
-            for (ObservacionesString observacion : obsList) {
-                Object[] rowData = {
-                        observacion.getIdObservacion(),
-                        observacion.getEstudiante(),
-                        observacion.getDocente(),
-                        observacion.getIdPeriodo(),
-                        observacion.getFecha(),
-                        observacion.getDetalle(),
-                        observacion.getIdEstudiante()
-                };
-                tableModel.addRow(rowData);
+            for (ObservacionesString tipoCodigo : encargadosList) {
+                String Fecha = tipoCodigo.getFecha().substring(0, 11);
+                
+                tableModel.addRow(new Object[]{
+                    tipoCodigo.getIdObservacion(),
+                    tipoCodigo.getEstudiante(),
+                    tipoCodigo.getDocente(),
+                    tipoCodigo.getDetalle(),
+                    Fecha,
+                   
+                });
             }
+        })
+        .exceptionally(throwable -> {
+            throwable.printStackTrace();
+            return null;
         });
-    }
+}
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -88,8 +95,6 @@ public class Observaciones extends javax.swing.JPanel {
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         table1 = new View.ExampleTable.Table();
-        btnAdd = new View.BotonesText.Buttons();
-        btnEdit = new View.BotonesText.Buttons();
         btnDelete = new View.BotonesText.Buttons();
 
         lb.setText("Observaciones");
@@ -103,20 +108,6 @@ public class Observaciones extends javax.swing.JPanel {
             }
         ));
         jScrollPane1.setViewportView(table1);
-
-        btnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/icons/add1.png"))); // NOI18N
-        btnAdd.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnAddMouseClicked(evt);
-            }
-        });
-
-        btnEdit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/icons/edit.png"))); // NOI18N
-        btnEdit.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnEditMouseClicked(evt);
-            }
-        });
 
         btnDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/icons/delete.png"))); // NOI18N
         btnDelete.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -135,10 +126,6 @@ public class Observaciones extends javax.swing.JPanel {
                 .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(34, 34, 34))
         );
@@ -146,10 +133,7 @@ public class Observaciones extends javax.swing.JPanel {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -179,44 +163,64 @@ public class Observaciones extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAddMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddMouseClicked
-        // TODO add your handling code here:
+    private void btnDeleteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnDeleteMouseClicked
+       int selectedRow = table1.getSelectedRow();
         
-    }//GEN-LAST:event_btnAddMouseClicked
+        
 
-    private void btnEditMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditMouseClicked
-        // TODO add your handling code here:
-
-        int selectedRow = table1.getSelectedRow();
-
+        // Verificar si se ha seleccionado una fila
         if (selectedRow != -1) {
             // Obtener los datos de las columnas de la fila seleccionada
-
-           
-        } else {
+            Object id = table1.getValueAt(selectedRow, 0);
+                System.out.println(id);
             Message obj = new Message();
             obj.txtTitle.setText("Aviso");
-            obj.txtContent.setText("Debe seleccionar una fila.");
+            obj.txtContent.setText("¿Desea eliminar este registro?");
             obj.eventOK(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent ae) {
-                    System.out.println("Click OK");
+
+                    String endpointUrl = "https://expo2023-6f28ab340676.herokuapp.com/0bservaciones/delete";
+                    // Código para eliminar el registro de la API
+                    CompletableFuture<Boolean> deleteFuture = ControllerFull.DeleteApiAsync(endpointUrl, (int) id);
+
+                    // Manejar la respuesta de la API
+                    deleteFuture.thenAccept(deleted -> {
+                        if (deleted) {
+                            // Registro eliminado con éxito
+                            Message obj = new Message();
+                            obj.txtTitle.setText("Aviso");
+                            obj.txtContent.setText("Observacion eliminada");
+                            obj.eventOK(new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent ae) {
+
+                                    cargarDatosAsync();
+                                    deleteAllTableRows(table1);
+                                    GlassPanePopup.closePopupLast();
+                                }
+                            });
+                            GlassPanePopup.showPopup(obj);
+                        } else {
+                            // Ocurrió un error al eliminar el registro
+                            Message obj = new Message();
+                            obj.txtTitle.setText("Aviso");
+                            obj.txtContent.setText("Error al eliminar la Observacion, intente nuevamente.");
+                            obj.eventOK(new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent ae) {
+
+                                    GlassPanePopup.closePopupLast();
+                                }
+                            });
+                            GlassPanePopup.showPopup(obj);
+                        }
+                    });
+
                     GlassPanePopup.closePopupLast();
                 }
             });
             GlassPanePopup.showPopup(obj);
-
-        }
-    }//GEN-LAST:event_btnEditMouseClicked
-
-    private void btnDeleteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnDeleteMouseClicked
-        // TODO add your handling code here:
-
-        int selectedRow = table1.getSelectedRow();
-
-        // Verificar si se ha seleccionado una fila
-        if (selectedRow != -1) {
-            
 
         } else {
             Message obj = new Message();
@@ -236,9 +240,7 @@ public class Observaciones extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private View.BotonesText.Buttons btnAdd;
     private View.BotonesText.Buttons btnDelete;
-    private View.BotonesText.Buttons btnEdit;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lb;
